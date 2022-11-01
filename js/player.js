59aa27067c9fd007ca46b480d7f918b9b548c748
Alex <slowdsports@gmail.com>
2022-11-01 00:14:05 -0600

    Update player.js

--- js/player.js
+++ js/player.js
@@ -4,11 +4,9 @@ var playerInstance = jwplayer("player");
 playerInstance.setup({
     playlist: [
         {
-            //image: atob(getIMG),
             sources: [
                 {
                     default: false,
-                    type: "dash",
                     file: atob(source),
                     drm: {
                         clearkey: { keyId: atob(key), key: atob(key2) },
@@ -18,47 +16,16 @@ playerInstance.setup({
             ],
         },
     ],
-    //height: "50%",
-    width: "100%",
+    width: "80%",
+    height: "100%",
     aspectratio: "16:9",
-    stretching: "bestfit",
-    mediaid: "player",
-    mute: true,
-    autostart: true,
-    language: "es",
-    cast: {
-        appid: "player",
-        logo: "https://eduveel1.github.io/baleada/img/iRTVW_PLAYER.png",
-    },
+    mute: false,
+    autostart: false,
     logo: {
         file: "https://eduveel1.github.io/baleada/img/iRTVW_PLAYER.png",
         hide: "false",
         position: "top-left",
-    }
+    },
+    cast: {},
+    sharing: {},
 });
-// Preview Hack
-setTimeout(function () {
-    jwplayer().setMute(false);
-    jwplayer().setControls(true);
-}, 1000);
-jwplayer().onTime(function (object) {
-    if (object.position > object.duration - 1) {
-        jwplayer().pause();
-    }
-});
-function goFullscreen(id) {
-    // Get the element that we want to take into fullscreen mode
-    var element = document.getElementById("jwp");
-    // These function will not exist in the browsers that don't support fullscreen mode yet,
-    // so we'll have to check to see if they're available before calling them.
-    if (element.mozRequestFullScreen) {
-        // This is how to go into fullscren mode in Firefox
-        // Note the "moz" prefix, which is short for Mozilla.
-        element.mozRequestFullScreen();
-    } else if (element.webkitRequestFullScreen) {
-        // This is how to go into fullscreen mode in Chrome and Safari
-        // Both of those browsers are based on the Webkit project, hence the same prefix.
-        element.webkitRequestFullScreen();
-    }
-    // Hooray, now we're in fullscreen mode!
-}
