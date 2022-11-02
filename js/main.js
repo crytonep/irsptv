362649c6b9299c1a02bfc33a587ee9637451cee3
Alex <slowdsports@gmail.com>
2022-11-02 10:44:05 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -304,6 +304,10 @@ setTimeout(() => {
                 0: {
                     items: 1,
                 },
+                // breakpoint from 480 up
+                480: {
+                    items: 2,
+                },
                 // breakpoint from 768 up
                 768: {
                     items: 4,
