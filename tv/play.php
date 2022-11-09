7abf11afa4ee1e06ed527bcde51133f6c098dcd2
Alex <slowdsports@gmail.com>
2022-11-08 23:28:08 -0600

    Update play.php

--- tv/play.php
+++ tv/play.php
@@ -26,7 +26,7 @@ $type = $result['type'];
 </div>
 <div class="container">
     <div class="row">
-        <div class="col-8">
+        <div class="col-12">
             <a id="playerFake" href="https://abolishmentglum.com/mkd1fhhe?key=81193c57b7f58377107604b71a3e49aa" target="_blank">
                 <img class="img-fluid" src="<?=$app?>img/player_img.png" alt="">
             </a>
@@ -41,11 +41,6 @@ $type = $result['type'];
                     <iframe class="embed-responsive-item" width="100%" height="100%" src="ck.php?c=<?=$result['channelId']?>" frameborder="0" scrolling="no" allowfullscreen allow-encrypted-media></iframe>
                 <?php } ?>
             </div>
-        </div>
-        <div style="height: 600px" class="col-4">
-            <!-- Chat -->
-            <iframe src="https://www.twitch.tv/embed/slowdmelendez360/chat?darkpopout&parent=iraffle.live" height="100%" width="100%">
-            </iframe>
         </div>
         <div class="container" id="adBanner2">
             <?php include('../inc/ads/banner2.php'); //include('../inc/ads/popunder.php');?>
