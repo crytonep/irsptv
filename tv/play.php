486083930508af58c083c5db915863587ea3e9a2
Alex <slowdsports@gmail.com>
2022-11-08 22:59:12 -0600

    Update play.php

--- tv/play.php
+++ tv/play.php
@@ -25,21 +25,29 @@ $type = $result['type'];
     </div>
 </div>
 <div class="container">
-    <a id="playerFake" href="https://abolishmentglum.com/mkd1fhhe?key=81193c57b7f58377107604b71a3e49aa" target="_blank">
-        <img src="<?=$app?>img/player_img.png" alt="">
-    </a>
-    <div class="hidden" id="playerContainer">
-        <?php if($type == 11) {?>
-            <iframe width="100%" height="800px" src="ckm.php?c=<?=$result['channelId']?>" frameborder="0" scrolling="no" allowfullscreen allow-encrypted-media></iframe>
-            <?php }
-        elseif(isset($_GET['hls'])) {?>
-            <iframe width="100%" height="800px" src="hls.php?c=<?=$_GET['c']?>" frameborder="0" scrolling="no" allowfullscreen allow-encrypted-media></iframe>
-        <?php }
-        else{ ?>
-            <iframe width="100%" height="800px" src="ck.php?c=<?=$result['channelId']?>" frameborder="0" scrolling="no" allowfullscreen allow-encrypted-media></iframe>
-        <?php } ?>
-    </div>
-    <div class="container" id="adBanner2">
-        <?php include('../inc/ads/banner2.php'); include('../inc/ads/popunder.php');?>
+    <div class="row">
+        <div class="col-8">
+            <a id="playerFake" href="https://abolishmentglum.com/mkd1fhhe?key=81193c57b7f58377107604b71a3e49aa" target="_blank">
+                <img class="img-fluid" src="<?=$app?>img/player_img.png" alt="">
+            </a>
+            <div class="hidden embed-responsive embed-responsive-16by9" id="playerContainer" style="width: 100%; margin: auto; box-shadow: 2px 2px 8px 2px #2c2c2c;">
+                <?php if($type == 11) {?>
+                    <iframe class="embed-responsive-item" width="100%" height="100%" src="ckm.php?c=<?=$result['channelId']?>" frameborder="0" scrolling="no" allowfullscreen allow-encrypted-media></iframe>
+                    <?php }
+                elseif(isset($_GET['hls'])) {?>
+                    <iframe class="embed-responsive-item" width="100%" height="100%" src="hls.php?c=<?=$_GET['c']?>" frameborder="0" scrolling="no" allowfullscreen allow-encrypted-media></iframe>
+                <?php }
+                else{ ?>
+                    <iframe class="embed-responsive-item" width="100%" height="100%" src="ck.php?c=<?=$result['channelId']?>" frameborder="0" scrolling="no" allowfullscreen allow-encrypted-media></iframe>
+                <?php } ?>
+            </div>
+        </div>
+        <div class="col-4">
+            <!-- Chat -->
+            <iframe width="100%" src="https://www.youtube.com/live_chat?v=uDn8r0q_oi4&embed_domain=localhost" ></iframe>
+        </div>
+        <div class="container" id="adBanner2">
+            <?php include('../inc/ads/banner2.php'); //include('../inc/ads/popunder.php');?>
+        </div>
     </div>
 </div>
