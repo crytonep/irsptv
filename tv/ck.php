f07e91342cbbf9559300e76a00b6f702ef3e3397
Alex <slowdsports@gmail.com>
2022-11-08 23:51:59 -0600

    Update ck.php

--- tv/ck.php
+++ tv/ck.php
@@ -1,76 +1,88 @@
-<script src="https://ssl.p.jwpcdn.com/player/v/8.24.0/jwplayer.js"></script>
-<script>jwplayer.key = 'XSuP4qMl+9tK17QNb+4+th2Pm9AWgMO/cYH8CI0HGGr7bdjo';</script>
-<div id="player"></div>
 <?php
 include('../conn.php');
 $canal = $_GET['c'];
 $query = mysqli_query($conn, "select * from channels
 where channelId='" . $canal . "'");
 $result = mysqli_fetch_assoc($query);
-// M+
-if ($result['type'] == 12) {
-    $main = "https://crytonep.herokuapp.com/https://";
-    $str = "-dash-"."mov"."ist"."arp"."lus".".em"."isi"."ond"."of6".".com"."/da"."sh/";
-    $ext = ".is"."ml/"."man"."ife"."st."."m"."p"."d";
-    $src = $result['channelUrl'];
-    $source = $main . $src . $str . $src . $ext;
-    $source = base64_encode($source);
-    $key = base64_encode($result['key1']);
-    $key2 = base64_encode($result['key2']);
+// DVTOTT
+if (strpos($result['channelUrl'], "//dtvott-") !== false){
+    $nURL = "https://irpc.ga/mg/play/?get=";
+    $url = base64_encode($result['channelUrl']);
+    $url = $nURL.$url."&key=".$result['key1']."&key2=".$result['key2'];
+    echo '
+    <iframe src="'.$url.'" width="100%" height="100%" scrolling="no" allowfullscreen allow-encrypted-media></iframe>
+    ';
 } else {
-    $source = base64_encode($result['channelUrl']);
-    $key = $result['key1'];
-    $key2 = $result['key2'];
-}
-echo '
-    <script>
-    let source = "' . $source . '";
-    var playerInstance = jwplayer("player");
-
-playerInstance.setup({
-    playlist: [
-        {
-            //image: atob(getIMG),
-            sources: [
-                {
-                    default: false,
-                    type: "dash",
-                    file: atob(source),
-                    drm: {
-                        clearkey: { keyId: atob("' . $key . '"), key: atob("' . $key2 . '") },
-                    },
-                    label: "0",
-                },
-            ],
-        },
-    ],
-    height: "100vh",
-    width: "100%",
-    aspectratio: "16:9",
-    stretching: "bestfit",
-    mediaid: "player",
-    mute: false,
-    autostart: true,
-    language: "es",
-    cast: {
-        appid: "player",
-        logo: "https://eduveel1.github.io/baleada/img/iRTVW_PLAYER.png",
-    },
-    logo: {
-        file: "https://eduveel1.github.io/baleada/img/iRTVW_PLAYER.png",
-        hide: "false",
-        position: "top-left",
+?>
+    <script src="https://ssl.p.jwpcdn.com/player/v/8.24.0/jwplayer.js"></script>
+    <script>jwplayer.key = 'XSuP4qMl+9tK17QNb+4+th2Pm9AWgMO/cYH8CI0HGGr7bdjo';</script>
+    <div id="player"></div>
+    <?php
+    // M+
+    if ($result['type'] == 12) {
+        $main = "https://crytonep.herokuapp.com/https://";
+        $str = "-dash-"."mov"."ist"."arp"."lus".".em"."isi"."ond"."of6".".com"."/da"."sh/";
+        $ext = ".is"."ml/"."man"."ife"."st."."m"."p"."d";
+        $src = $result['channelUrl'];
+        $source = $main . $src . $str . $src . $ext;
+        $source = base64_encode($source);
+        $key = base64_encode($result['key1']);
+        $key2 = base64_encode($result['key2']);
+    } else {
+        $source = base64_encode($result['channelUrl']);
+        $key = $result['key1'];
+        $key2 = $result['key2'];
     }
-});
-// Preview Hack
-setTimeout(function() {
-    jwplayer().setMute(false);
-    jwplayer().setControls(true);
-    },1000);
-    jwplayer().onTime(function(object) {
-        if (object.position > object.duration - 1) {
-            jwplayer().pause();
+    echo '
+        <script>
+        let source = "' . $source . '";
+        var playerInstance = jwplayer("player");
+
+    playerInstance.setup({
+        playlist: [
+            {
+                //image: atob(getIMG),
+                sources: [
+                    {
+                        default: false,
+                        type: "dash",
+                        file: atob(source),
+                        drm: {
+                            clearkey: { keyId: atob("' . $key . '"), key: atob("' . $key2 . '") },
+                        },
+                        label: "0",
+                    },
+                ],
+            },
+        ],
+        height: "100vh",
+        width: "100%",
+        aspectratio: "16:9",
+        stretching: "bestfit",
+        mediaid: "player",
+        mute: false,
+        autostart: true,
+        language: "es",
+        cast: {
+            appid: "player",
+            logo: "https://eduveel1.github.io/baleada/img/iRTVW_PLAYER.png",
+        },
+        logo: {
+            file: "https://eduveel1.github.io/baleada/img/iRTVW_PLAYER.png",
+            hide: "false",
+            position: "top-left",
         }
     });
-    </script>
-    ';
+    // Preview Hack
+    setTimeout(function() {
+        jwplayer().setMute(false);
+        jwplayer().setControls(true);
+        },1000);
+        jwplayer().onTime(function(object) {
+            if (object.position > object.duration - 1) {
+                jwplayer().pause();
+            }
+        });
+        </script>
+        ';
+}
