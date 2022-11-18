06905e76acbb25d9e3a3091174d8d6c1a77f4427
Alex <slowdsports@gmail.com>
2022-11-17 18:48:48 -0600

    Update play.php

--- tv/play.php
+++ tv/play.php
@@ -85,10 +85,8 @@ if (isset($_GET['ifr'])) {
         </div>
     </div>
     <div class="col-md-4 col-sm-12">
-        <div class="embed-responsive embed-responsive-16by9" id="chat">
-            <iframe class="hidden" id="twitch-chat-embed" src="https://www.twitch.tv/embed/iraffelive/chat?darkpopout&parent=iraffle.live" height="100%" width="100%">
+        <iframe class="hidden" id="twitch-chat-embed" src="https://www.twitch.tv/embed/iraffelive/chat?darkpopout&parent=iraffle.live" height="100%" width="100%">
         </iframe>
-        </div>
     </div>
 </div>
 <hr>
