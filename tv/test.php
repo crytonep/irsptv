8513f36139ce555ddeff8ae6046141d5f23492b6
Alex <slowdsports@gmail.com>
2022-11-18 17:22:38 -0600

    Update test.php

--- tv/test.php
+++ tv/test.php
@@ -1,3 +1,76 @@
+<script src="https://ssl.p.jwpcdn.com/player/v/8.24.0/jwplayer.js"></script>
+<?php
+echo '
 <script>
-    window.top.location.href = "https://irpc.ga/mg/play/sandbox.php?r=Ly9nZXJtYW55aXAud29yay9zdi1tdC5waHA/Z2V0PUx5OWtkSFp2ZEhRdFkySmpMbUZyWVcxaGFYcGxaQzV1WlhRdlpHRnphRjlzYVhabFh6RXdPREV2YldGdWFXWmxjM1F1YlhCayZrZXk9WkRNeVpEZ3hOV1F4WmpkbU5UTXlOV0poTnpNMU5qSTFNelEyTmpKak9UST0ma2V5Mj1OelZsTmpaaE5HVmtOemc0TUdNM01UWTFOV1F5TkdVMU1tTmhaRE5pWkRNPQ==";
+var source = "' . base64_encode($_GET['r']) . '";
+var key = "' . base64_encode($_GET['key']) . '";
+var key2 = "' . base64_encode($_GET['key2']) . '";
+var img = "' . base64_encode($_GET['img']) . '";
+</script>'; ?>
+<div id="player"></div>
+<script>
+    jwplayer.key = 'XSuP4qMl+9tK17QNb+4+th2Pm9AWgMO/cYH8CI0HGGr7bdjo';
+    var playerInstance = jwplayer("player");
+
+    playerInstance.setup({
+        playlist: [{
+            image: atob(img),
+            sources: [{
+                default: false,
+                type: "dash",
+                file: atob(source),
+                drm: {
+                    clearkey: {
+                        keyId: atob(key),
+                        key: atob(key2)
+                    },
+                },
+                label: "0",
+            }, ],
+        }, ],
+        //height: "50%",
+        width: "100%",
+        aspectratio: "16:9",
+        stretching: "bestfit",
+        mediaid: "player",
+        mute: true,
+        autostart: false,
+        language: "es",
+        cast: {
+            appid: "player",
+            logo: "https://eduveel1.github.io/baleada/img/iRTVW_PLAYER.png",
+        },
+        logo: {
+            file: "https://eduveel1.github.io/baleada/img/iRTVW_PLAYER.png",
+            hide: "false",
+            position: "top-left",
+        }
+    });
+    // Preview Hack
+    setTimeout(function() {
+        jwplayer().setMute(false);
+        jwplayer().setControls(true);
+    }, 1000);
+    jwplayer().onTime(function(object) {
+        if (object.position > object.duration - 1) {
+            jwplayer().pause();
+        }
+    });
+
+    function goFullscreen(id) {
+        // Get the element that we want to take into fullscreen mode
+        var element = document.getElementById("jwp");
+        // These function will not exist in the browsers that don't support fullscreen mode yet,
+        // so we'll have to check to see if they're available before calling them.
+        if (element.mozRequestFullScreen) {
+            // This is how to go into fullscren mode in Firefox
+            // Note the "moz" prefix, which is short for Mozilla.
+            element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullScreen) {
+            // This is how to go into fullscreen mode in Chrome and Safari
+            // Both of those browsers are based on the Webkit project, hence the same prefix.
+            element.webkitRequestFullScreen();
+        }
+        // Hooray, now we're in fullscreen mode!
+    }
 </script>
